f2ff90bd2ffb12ceecb6facddbcbaa14564b6db9
Luciano José <luciano.jsjr@gmail.com>
2017-04-27 22:54:34 -0300

    Adjust Location Manager and Change Presentation Text

--- app/src/main/java/com/petbooking/Managers/LocationManager.java
+++ app/src/main/java/com/petbooking/Managers/LocationManager.java
@@ -9,15 +9,12 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.gson.Gson;
-import com.petbooking.Models.User;
 import com.petbooking.Models.UserAddress;
 import com.petbooking.R;
 import com.petbooking.Utils.CommonUtils;
@@ -140,7 +137,6 @@ public class LocationManager implements GoogleApiClient.ConnectionCallbacks,
             if (addresses.size() > 0) {
                 Address address = addresses.get(0);
                 cityState = address.getLocality() + " - " + address.getAdminArea();
-                Log.d("ADDRESS", new Gson().toJson(address));
             }
         } catch (IOException e) {
             return cityState;

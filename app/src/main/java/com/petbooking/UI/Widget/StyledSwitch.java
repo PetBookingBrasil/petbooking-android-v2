2e9ced9908adbee71477ef7ccfe7abf6d4706564
Luciano José <luciano.jsjr@gmail.com>
2017-06-05 22:44:07 -0300

    Add Title Attribute and Event on View Click

--- app/src/main/java/com/petbooking/UI/Widget/StyledSwitch.java
+++ app/src/main/java/com/petbooking/UI/Widget/StyledSwitch.java
@@ -2,7 +2,6 @@ package com.petbooking.UI.Widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -18,8 +17,17 @@ import com.petbooking.R;
 
 public class StyledSwitch extends RelativeLayout {
 
+    private RelativeLayout mView;
     private TextView mTvTitle;
     private SwitchButton mSwitch;
+
+    OnClickListener viewListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            boolean isChecked = mSwitch.isChecked();
+            mSwitch.setChecked(!isChecked);
+        }
+    };
 
     public StyledSwitch(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -29,12 +37,31 @@ public class StyledSwitch extends RelativeLayout {
     private void init(Context context, AttributeSet attrs) {
         View view = View.inflate(context, R.layout.widget_styled_switch, this);
 
+        mView = (RelativeLayout) view.findViewById(R.id.switchView);
         mTvTitle = (TextView) view.findViewById(R.id.switch_title);
         mSwitch = (SwitchButton) view.findViewById(R.id.custom_switch);
 
+        if (attrs != null) {
+            TypedArray prop = context.getTheme()
+                    .obtainStyledAttributes(attrs, R.styleable.StyledSwitch, 0, 0);
+
+            String title = "";
+
+            try {
+                title = prop.getString(R.styleable.StyledSwitch_title);
+            } catch (Exception e) {
+                Log.e("SWITCH", "There was an error loading attributes.");
+            } finally {
+                prop.recycle();
+            }
+
+
+            setTitle(title);
+            mView.setOnClickListener(viewListener);
+        }
     }
 
-    public void setTitle(String title){
+    public void setTitle(String title) {
         mTvTitle.setText(title);
     }
 

8396d81b17faa7ffa4f8a64009a72b933a6f080c
Luciano José <luciano.jsjr@gmail.com>
2017-06-13 23:49:51 -0300

    Start to Update Business Info

--- app/src/main/java/com/petbooking/UI/Dashboard/BusinessMap/BusinessMap.java
+++ app/src/main/java/com/petbooking/UI/Dashboard/BusinessMap/BusinessMap.java
@@ -6,16 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.petbooking.API.Business.BusinessService;
+import com.petbooking.Interfaces.APICallback;
+import com.petbooking.Managers.LocationManager;
+import com.petbooking.Managers.SessionManager;
+import com.petbooking.Models.Business;
 import com.petbooking.R;
 import com.petbooking.Utils.AppUtils;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,18 +31,53 @@ import com.petbooking.Utils.AppUtils;
 public class BusinessMap extends Fragment implements OnMapReadyCallback {
 
     private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private BusinessService mBusinessService;
+    private LocationManager mLocationManager;
+    private String userId;
     private MapView mMapView;
     private GoogleMap mGMap = null;
+    private ArrayList<Business> mBusinessList;
+    private Business selectedBusiness;
+
+    /**
+     * Business Elements
+     */
+    private View mBusinessLayout;
+    GoogleMap.OnMapClickListener mapListener = new GoogleMap.OnMapClickListener() {
+        @Override
+        public void onMapClick(LatLng latLng) {
+            mBusinessLayout.setVisibility(View.GONE);
+        }
+    };
+    private TextView mTvName;
+    GoogleMap.OnMarkerClickListener markerListener = new GoogleMap.OnMarkerClickListener() {
+        @Override
+        public boolean onMarkerClick(Marker marker) {
+            int index = Integer.parseInt(marker.getSnippet());
+            selectedBusiness = mBusinessList.get(index);
+
+            updateBusinessInfo(selectedBusiness);
+
+            return true;
+        }
+    };
 
     public BusinessMap() {
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_business_map, container, false);
         Bundle mapViewBundle = null;
+
+        mBusinessService = new BusinessService();
+        mLocationManager = LocationManager.getInstance();
+        mBusinessList = new ArrayList<>();
+        userId = SessionManager.getInstance().getUserLogged().id;
+
+        mBusinessLayout = view.findViewById(R.id.business_layout);
+        mTvName = (TextView) view.findViewById(R.id.business_name);
 
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
@@ -44,6 +87,7 @@ public class BusinessMap extends Fragment implements OnMapReadyCallback {
         mMapView.onCreate(mapViewBundle);
 
         mMapView.getMapAsync(this);
+        listBusiness();
 
         return view;
     }
@@ -51,12 +95,8 @@ public class BusinessMap extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGMap = googleMap;
-        LatLng center = new LatLng(-8.0578381, -34.8828969);
-        MarkerOptions markerOptions = new MarkerOptions()
-                .position(center)
-                .icon(BitmapDescriptorFactory.fromBitmap(AppUtils.getBitmap(getContext(), R.drawable.ic_marker)));
-        googleMap.addMarker(markerOptions);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 16));
+        mGMap.setOnMarkerClickListener(markerListener);
+        mGMap.setOnMapClickListener(mapListener);
     }
 
     @Override
@@ -95,4 +135,54 @@ public class BusinessMap extends Fragment implements OnMapReadyCallback {
         super.onLowMemory();
         mMapView.onLowMemory();
     }
+
+    /**
+     * List Business
+     */
+    public void listBusiness() {
+        mBusinessService.listBusiness(mLocationManager.getLocationCoords(), userId, 1, 30, new APICallback() {
+            @Override
+            public void onSuccess(Object response) {
+                mBusinessList = (ArrayList<Business>) response;
+                createMarkers(mBusinessList);
+            }
+
+            @Override
+            public void onError(Object error) {
+
+            }
+        });
+    }
+
+    /**
+     * Create Markes from Business list
+     *
+     * @param businessList
+     */
+    public void createMarkers(ArrayList<Business> businessList) {
+        int index = 0;
+        for (Business business : businessList) {
+            if (business.latitude != null && business.longitude != null) {
+                LatLng position = new LatLng(business.latitude, business.longitude);
+                Marker marker = mGMap.addMarker(new MarkerOptions()
+                        .title(business.name)
+                        .snippet(String.valueOf(index))
+                        .position(position)
+                        .icon(BitmapDescriptorFactory.fromBitmap(AppUtils.getBitmap(getContext(), R.drawable.ic_marker))));
+
+            }
+            index++;
+        }
+    }
+
+    /**
+     * Update Business Info
+     * when Click on Mark
+     * @param business
+     */
+    public void updateBusinessInfo(Business business){
+        mTvName.setText(business.name);
+        mBusinessLayout.setVisibility(View.VISIBLE);
+    }
+
 }

660d1ddd394db57c7d54a8a3ef1c9577bd9c2a61
Luciano José <luciano.jsjr@gmail.com>
2017-08-06 17:26:42 -0300

    Create Detail Activity

---
+++ app/src/main/java/com/petbooking/UI/Dashboard/Business/ServiceDetail/ServiceDetailActivity.java
@@ -0,0 +1,15 @@
+package com.petbooking.UI.Dashboard.Business.ServiceDetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.petbooking.R;
+
+public class ServiceDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_detail);
+    }
+}

844c931446e5693d0617c91c0eb7cbd8e686263f
Luciano José <luciano.jsjr@gmail.com>
2017-08-08 23:35:09 -0300

    Show Appointment Content

--- app/src/main/java/com/petbooking/UI/Dashboard/Business/ServiceDetail/AppointmentBottomFragment.java
+++ app/src/main/java/com/petbooking/UI/Dashboard/Business/ServiceDetail/AppointmentBottomFragment.java
@@ -7,12 +7,16 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.google.gson.Gson;
 import com.petbooking.API.Appointment.AppointmentService;
 import com.petbooking.API.Appointment.Models.ProfessionalResp;
 import com.petbooking.Interfaces.APICallback;
@@ -30,9 +34,17 @@ public class AppointmentBottomFragment extends BottomSheetDialogFragment {
     private LinearLayout mFragmentLayout;
 
     /**
+     * Buttons
+     */
+    private ImageView mIvPreviousDate;
+    private ImageView mIvNextDate;
+    private Button mBtnConfirmAppointment;
+
+    /**
      * Professional Components
      */
     private int selectedProfessional = -1;
+    private LinearLayoutManager mProfessionalLayout;
     private ArrayList<Professional> mProfessionalList;
     private ProfessionalListAdapter mProfessionalAdapter;
     private RecyclerView mRvProfessional;
@@ -40,17 +52,29 @@ public class AppointmentBottomFragment extends BottomSheetDialogFragment {
     /**
      * Dates Components
      */
-    private int selectedMonth = -1;
+    private int selectedMonth = 0;
+    private ArrayList<AppointmentDate> mDateList;
+    private LinearLayoutManager mDateLayout;
     private RelativeLayout mDateListLayout;
     private DateListAdapter mDateListAdapter;
     private RecyclerView mRvAppointmentDate;
     private SnapHelper mDateSnapHelper;
 
     /**
-     * Dates Components
+     * Days Components
      */
+    private int selectedDay = -1;
+    private LinearLayoutManager mDaysLayout;
     private DaysListAdapter mDaysListAdapter;
     private RecyclerView mRvDays;
+
+    /**
+     * Time Components
+     */
+    private int selectedTime = -1;
+    private LinearLayoutManager mTimeLayout;
+    private RecyclerView mRvTime;
+    private TimeListAdapter mTimeAdapter;
 
     ProfessionalListAdapter.OnSelectProfessionaListener professionaListener = new ProfessionalListAdapter.OnSelectProfessionaListener() {
         @Override
@@ -62,6 +86,64 @@ public class AppointmentBottomFragment extends BottomSheetDialogFragment {
         }
     };
 
+    View.OnClickListener btnDateListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int itemId = v.getId();
+            if (itemId == mIvPreviousDate.getId()) {
+                previousMonth();
+            } else {
+                nextMonth();
+            }
+
+            if (selectedMonth != -1) {
+                mDaysListAdapter.updateList(mDateList.get(selectedMonth).days);
+                mDaysListAdapter.notifyDataSetChanged();
+                mDaysListAdapter.resetDay();
+            }
+        }
+    };
+
+    DaysListAdapter.OnSelectDayListener dayListener = new DaysListAdapter.OnSelectDayListener() {
+        @Override
+        public void onSelect(int position) {
+            selectedDay = position;
+            handleSelectDay(selectedDay);
+        }
+    };
+
+    TimeListAdapter.OnSelectTimeListener timeListener = new TimeListAdapter.OnSelectTimeListener() {
+        @Override
+        public void onSelect(int position) {
+            if (position != -1) {
+                mBtnConfirmAppointment.setEnabled(true);
+            } else {
+                mBtnConfirmAppointment.setEnabled(false);
+            }
+        }
+    };
+
+    RecyclerView.OnScrollListener dateScroll = new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            super.onScrollStateChanged(recyclerView, newState);
+            if (newState == RecyclerView.SCROLL_STATE_IDLE && mDateLayout.findFirstCompletelyVisibleItemPosition() != -1) {
+                int itemPosition = mDateLayout.findFirstCompletelyVisibleItemPosition();
+                selectedMonth = itemPosition;
+
+                if (selectedMonth != -1) {
+                    mDaysListAdapter.updateList(mDateList.get(selectedMonth).days);
+                    mDaysListAdapter.notifyDataSetChanged();
+                    mDaysListAdapter.resetDay();
+                }
+            }
+        }
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+        }
+    };
 
     @Nullable
     @Override
@@ -71,20 +153,35 @@ public class AppointmentBottomFragment extends BottomSheetDialogFragment {
 
         mAppointmentService = new AppointmentService();
         mProfessionalList = new ArrayList<>();
+        mDateList = new ArrayList<>();
         listProfessional();
 
-        LinearLayoutManager mProfessionalLayout = new LinearLayoutManager(getContext());
+        mProfessionalLayout = new LinearLayoutManager(getContext());
         mProfessionalLayout.setOrientation(LinearLayout.HORIZONTAL);
 
-        LinearLayoutManager mDateLayout = new LinearLayoutManager(getContext());
+        mDateLayout = new LinearLayoutManager(getContext());
         mDateLayout.setOrientation(LinearLayout.HORIZONTAL);
 
-        LinearLayoutManager mDaysLayout = new LinearLayoutManager(getContext());
+        mDaysLayout = new LinearLayoutManager(getContext());
         mDaysLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        mTimeLayout = new LinearLayoutManager(getContext());
+        mTimeLayout.setOrientation(LinearLayout.HORIZONTAL);
 
         mProfessionalAdapter = new ProfessionalListAdapter(getContext(), mProfessionalList, professionaListener);
         mDateListAdapter = new DateListAdapter(getContext(), new ArrayList<AppointmentDate>());
-        mDaysListAdapter = new DaysListAdapter(getContext(), new ArrayList<ProfessionalResp.Slot>());
+        mDaysListAdapter = new DaysListAdapter(getContext(), new ArrayList<ProfessionalResp.Slot>(), dayListener);
+        mTimeAdapter = new TimeListAdapter(getContext(), new ArrayList<String>(), timeListener);
+
+        /**
+         * Buttons
+         */
+        mIvPreviousDate = (ImageView) view.findViewById(R.id.previous_date);
+        mIvNextDate = (ImageView) view.findViewById(R.id.next_date);
+        mBtnConfirmAppointment = (Button) view.findViewById(R.id.confirm_appointment);
+
+        mIvPreviousDate.setOnClickListener(btnDateListener);
+        mIvNextDate.setOnClickListener(btnDateListener);
 
         /**
          * Professional Recyclerview
@@ -102,17 +199,30 @@ public class AppointmentBottomFragment extends BottomSheetDialogFragment {
         mRvAppointmentDate = (RecyclerView) view.findViewById(R.id.date_list);
         mRvAppointmentDate.setHasFixedSize(true);
         mRvAppointmentDate.setLayoutManager(mDateLayout);
+        mRvAppointmentDate.addOnScrollListener(dateScroll);
         mDateSnapHelper.attachToRecyclerView(mRvAppointmentDate);
 
- /*       *//**
+        /**
          * RecyclerView Days
-         *//*
+         */
         mRvDays = (RecyclerView) view.findViewById(R.id.day_list);
         mRvDays.setHasFixedSize(true);
-        mRvDays.setLayoutManager(mDaysLayout);*/
+        mRvDays.setLayoutManager(mDaysLayout);
+
+
+        /**
+         * Time List
+         */
+        mRvTime = (RecyclerView) view.findViewById(R.id.time_list);
+        mRvTime.setHasFixedSize(true);
+        mRvTime.setLayoutManager(mTimeLayout);
+
 
         mRvProfessional.setAdapter(mProfessionalAdapter);
         mRvAppointmentDate.setAdapter(mDateListAdapter);
+        mRvDays.setAdapter(mDaysListAdapter);
+        mRvTime.setAdapter(mTimeAdapter);
+
 
         return view;
     }
@@ -154,10 +264,49 @@ public class AppointmentBottomFragment extends BottomSheetDialogFragment {
      */
     private void handleSelectProfessional() {
         Professional professional = mProfessionalList.get(selectedProfessional);
+        mDateList = professional.availableDates;
 
-        mDateListAdapter.updateList(professional.availableDates);
+        mDateListAdapter.updateList(mDateList);
         mDateListAdapter.notifyDataSetChanged();
 
+        mDaysListAdapter.updateList(mDateList.get(selectedMonth).days);
+        mDaysListAdapter.notifyDataSetChanged();
+
         mDateListLayout.setVisibility(View.VISIBLE);
+        mRvDays.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Show Times on select day
+     */
+    private void handleSelectDay(int position) {
+        if (position != -1) {
+            mTimeAdapter.resetTime();
+            mTimeAdapter.updateList(mDateList.get(selectedMonth).days.get(position).times);
+            mTimeAdapter.notifyDataSetChanged();
+            mRvTime.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Select the previous day
+     */
+    public void previousMonth() {
+        this.selectedMonth = mDateLayout.findFirstVisibleItemPosition();
+        if (this.selectedMonth > 0) {
+            selectedMonth--;
+            mRvAppointmentDate.smoothScrollToPosition(selectedMonth);
+        }
+    }
+
+    /**
+     * Select the next day
+     */
+    public void nextMonth() {
+        this.selectedMonth = mDateLayout.findFirstVisibleItemPosition();
+        if (this.selectedMonth < (mDateList.size() - 1)) {
+            selectedMonth++;
+            mRvAppointmentDate.smoothScrollToPosition(selectedMonth);
+        }
     }
 }

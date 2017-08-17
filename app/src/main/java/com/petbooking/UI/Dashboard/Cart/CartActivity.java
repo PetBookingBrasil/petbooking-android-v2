a7fa36fa707a4593c8542170321f301b09e1cf4f
Luciano José <luciano.jsjr@gmail.com>
2017-08-16 23:43:57 -0300

    Create Cart Activity

---
+++ app/src/main/java/com/petbooking/UI/Dashboard/Cart/CartActivity.java
@@ -0,0 +1,15 @@
+package com.petbooking.UI.Dashboard.Cart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.petbooking.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}

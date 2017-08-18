dee3361a5f3ca156abc47e0a1c2a6d63960817ae
Luciano José <luciano.jsjr@gmail.com>
2017-08-17 23:59:31 -0300

    Update cart activity title

--- app/src/main/java/com/petbooking/UI/Dashboard/Cart/CartActivity.java
+++ app/src/main/java/com/petbooking/UI/Dashboard/Cart/CartActivity.java
@@ -3,13 +3,21 @@ package com.petbooking.UI.Dashboard.Cart;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.petbooking.Managers.AppointmentManager;
 import com.petbooking.R;
 
 public class CartActivity extends AppCompatActivity {
+
+    private AppointmentManager mAppointmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        mAppointmentManager = AppointmentManager.getInstance();
+
+        getSupportActionBar().setTitle(R.string.cart_title);
+        getSupportActionBar().setSubtitle(mAppointmentManager.getCurrentBusinessName());
     }
 }

66f173fbfa9ed23d2e0451e893680598f043c868
Bruno Tortato Furtado <bruno@furtado.me>
2017-10-18 17:55:23 -0200

    [Added] Fake models to test the view behavior

--- app/src/main/java/com/petbooking/Models/Artist.java
+++ app/src/main/java/com/petbooking/Models/Artist.java
@@ -1,8 +1,57 @@
 package com.petbooking.Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
- * Created by bfurtado on 18/10/17.
+ * Created by Bruno Tortato Furtado on 18/10/17.
  */
 
-public class Artist {
+public final class Artist implements Parcelable {
+
+    private String title;
+
+    public Artist(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    //region - Parcelable
+
+    protected Artist(Parcel in) {
+        title = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+    }
+
+    public static final Parcelable.Creator<Artist> CREATOR = new Parcelable.Creator<Artist>() {
+
+        @Override
+        public Artist createFromParcel(Parcel source) {
+            return new Artist(source);
+        }
+
+        @Override
+        public Artist[] newArray(int size) {
+            return new Artist[size];
+        }
+    };
+
+    //endregion
+
 }
--- app/src/main/java/com/petbooking/Models/Genre.java
+++ app/src/main/java/com/petbooking/Models/Genre.java
@@ -1,8 +1,17 @@
 package com.petbooking.Models;
 
+import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup;
+
+import java.util.List;
+
 /**
- * Created by bfurtado on 18/10/17.
+ * Created by Bruno Tortato Furtado on 18/10/17.
  */
 
-public class Genre {
+public final class Genre extends ExpandableGroup<Artist> {
+
+    public Genre(String title, List<Artist> items) {
+        super(title, items);
+    }
+
 }

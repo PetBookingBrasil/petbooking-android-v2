d70af9b8ba8bcbd847d9e210efd895582e29b1a0
Bruno Tortato Furtado <bruno@furtado.me>
2017-11-18 12:14:32 -0200

    [Removed] Logs

--- app/src/main/java/com/petbooking/UI/Dashboard/Business/BusinessActivity.java
+++ app/src/main/java/com/petbooking/UI/Dashboard/Business/BusinessActivity.java
@@ -128,15 +128,6 @@ public class BusinessActivity extends AppCompatActivity implements ConfirmDialog
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Log.i("BRUNO", "resultCode: " + resultCode);
-        Log.i("BRUNO", "requestCode: " + requestCode);
-
-    }
-
     //endregion
 
     //region - Public
